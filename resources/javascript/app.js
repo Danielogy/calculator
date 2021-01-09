9db37bee0a2d4d093fd89389b65c99876e1899fd
Danielogy <danielfra1223@yahoo.com>
2021-01-08 22:17:21 -0500

    added keyboard support and added functionality

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -1,5 +1,6 @@
 const displayValue = document.querySelector('#value');
 const btns = document.querySelectorAll('button');
+const dot = document.querySelector('#dot');
 let expression = {
     num1: null,
     num2: null,
@@ -8,29 +9,38 @@ let expression = {
 
 btns.forEach(btn =>{
     btn.addEventListener('click',  e =>{
-       let that = e.target.id;  
-       const exp = displayValue.textContent += e.target.value;
+       let that = e.target.id;
+       const operands = displayValue.textContent += e.target.value;
+       displayValue.style.cssText = 'font-size: 3rem';
 
        if(that == 'mult' || that == 'divide' || that == 'plus' || that == 'minus' || that == 'mod'){
             clear();
             expression.operator = that;
             if(expression['num1'] == null)
-                expression['num1'] = exp; 
+                expression['num1'] = operands;      
        }
 
        if(that == 'equals' && (expression['num1'] != null)){
-           expression['num2'] = exp;
-           displayValue.textContent = operate(expression.operator, Number(expression.num1), Number(expression.num2));
+           expression['num2'] = operands;
+           let answer = operate(expression.operator, Number(expression.num1), Number(expression.num2));
+           answer.toString().length = 0;
+           
+           resizeDisplay(answer);
+
            expression['num1'] = displayValue.textContent;
            expression['num2'] = null;
-           console.log(expression);
+
        }
-       console.log(expression);
-       if(e.target.id == 'ac')
+
+       if(displayValue.textContent.includes('.') && that == 'dot'){
+            btn.disabled = true;
+       }
+
+        if(that == 'ac')
             clear();
 
-        if(e.target.id == 'plus-minus' && displayValue.textContent != '')
-            displayValue.textContent = convertSign(exp);
+        if(that == 'plus-minus' && displayValue.textContent != '')
+            displayValue.textContent = convertSign(displayValue.textContent);
     });
 });
 
@@ -39,17 +49,23 @@ function clear(){
     expression.num1 = null;
     expression.num2 = null;
     expression.operator = null;
+    dot.disabled = false;
 }
 
 function convertSign(exp){
+    Number(exp);
     return (-exp);
 }
 
 function operate(operator, num1, num2){
     if(operator == 'mod')
         return num1 % num2;
-    else if(operator == 'divide')
-        return num1 / num2
+    else if(operator == 'divide'){
+        if(num2 != 0)
+            return num1 / num2;
+        else
+            displayValue.textContent = "Uhhhh....";
+    }
     else if(operator == 'mult')
         return num1 * num2;
     else if(operator == 'minus')
@@ -58,3 +74,63 @@ function operate(operator, num1, num2){
         return num1 + num2;
 }
 
+window.addEventListener('keydown', e =>{
+    if(e.key >= 0 && e.key <= 9)  
+        displayValue.textContent += e.key;
+    if(e.key === ".")
+        displayValue.textContent += e.key;
+    if(e.key === "=" || e.key === "Enter"){
+        if(expression['num1'] != null){
+            expression['num2'] = displayValue.textContent;
+            let answer = operate(expression.operator, Number(expression.num1), Number(expression.num2));
+            resizeDisplay(answer);
+        }
+    }
+    if(e.key === "Escape") 
+        clear();
+    if(e.key === "Backspace")
+        displayValue.textContent = displayValue.textContent.toString().slice(0, -1);
+    if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
+        setOperator(e.key); 
+});
+
+function setOperator(key) {
+    if(key === "/"){
+        expression.operator = "divide";
+        readyToMath();
+    }
+    if(key === "*"){
+        expression.operator = 'mult';
+        readyToMath();
+    }
+    if(key === "-"){
+        expression.operator = 'minus';
+        readyToMath();
+    }   
+    if(key === "+"){
+        expression.operator = 'add';
+        readyToMath()
+    }
+}
+
+function readyToMath(){
+    if(displayValue.textContent == '')
+        expression.operator = null;
+    else{
+        expression['num1'] = displayValue.textContent;
+        console.log(expression);
+        displayValue.textContent = '';
+    }
+}
+
+function resizeDisplay(answer){
+    if (answer.toString().length >= 15) {
+        let rounded = Math.round(answer * 100) / 100;
+        displayValue.textContent = rounded;
+        displayValue.style.cssText = 'font-size: 2rem';
+    } 
+    else {
+        displayValue.textContent = answer;
+        displayValue.style.cssText = 'font-size: 3rem';
+    }
+}
