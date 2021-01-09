7b83ad9ae17e60f044a4a418a200817270f77172
Danielogy <danielfra1223@yahoo.com>
2021-01-09 03:39:57 -0500

    finished project

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -1,6 +1,8 @@
 const displayValue = document.querySelector('#value');
 const btns = document.querySelectorAll('button');
 const dot = document.querySelector('#dot');
+
+//object to hold the first operand, second operand and operator
 let expression = {
     num1: null,
     num2: null,
@@ -9,10 +11,13 @@ let expression = {
 
 btns.forEach(btn =>{
     btn.addEventListener('click',  e =>{
+        //prevent redundancy in code
        let that = e.target.id;
+       
        const operands = displayValue.textContent += e.target.value;
-       displayValue.style.cssText = 'font-size: 3rem';
+       displayValue.style.cssText = 'font-size: 3rem'; 
 
+       //set the first operand of expressions object after hitting the operator
        if(that == 'mult' || that == 'divide' || that == 'plus' || that == 'minus' || that == 'mod'){
             clear();
             expression.operator = that;
@@ -20,16 +25,21 @@ btns.forEach(btn =>{
                 expression['num1'] = operands;      
        }
 
+       //get the answer based on the object's values.
        if(that == 'equals' && (expression['num1'] != null)){
+           //if the first num isn't null, set the second one to the display's value.
            expression['num2'] = operands;
            let answer = operate(expression.operator, Number(expression.num1), Number(expression.num2));
            answer.toString().length = 0;
            
+           //if the answer is too big, make the font smaller
            resizeDisplay(answer);
-
+            
+           //set the first number to the display value to get ready for the next operation
            expression['num1'] = displayValue.textContent;
        }
 
+       //if the display already has a '.', prevent the user from hitting it again
        if(displayValue.textContent.includes('.') && that == 'dot'){
             btn.disabled = true;
        }
@@ -37,11 +47,13 @@ btns.forEach(btn =>{
         if(that == 'ac')
             clear();
 
+        //get the display's value and return its negation
         if(that == 'plus-minus' && displayValue.textContent != '')
             displayValue.textContent = convertSign(displayValue.textContent);
     });
 });
 
+//clear the display and the object's values
 function clear(){
     displayValue.textContent = '';
     expression.num1 = null;
@@ -50,11 +62,13 @@ function clear(){
     dot.disabled = false;
 }
 
+//return the values negation
 function convertSign(exp){
     Number(exp);
     return (-exp);
 }
 
+//arithmetic function
 function operate(operator, num1, num2){
     if(operator == 'mod')
         return num1 % num2;
@@ -72,6 +86,7 @@ function operate(operator, num1, num2){
         return num1 + num2;
 }
 
+//keyboard support
 window.addEventListener('keydown', e =>{
     if(e.key >= 0 && e.key <= 9)  
         displayValue.textContent += e.key;
@@ -124,6 +139,8 @@ function readyToMath(){
     }
 }
 
+//if answer's value is longer than 15 digits in length,
+//make the font smaller and round. otherwise continue.
 function resizeDisplay(answer){
     if (answer.toString().length >= 15) {
         let rounded = Math.round(answer * 100) / 100;
